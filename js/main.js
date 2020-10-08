f225e70046dcd1e97321e26d6b393f0ac8288487
StanislasBASQUIN <stanislas.basquin@gmail.com>
2020-10-08 18:51:04 +0200

    Pushing active files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Create a form
+// Link the Submit button to the API
+
+// First Step : Extract movies
+// Second Step : Display movies with
+// Poster
+// Name
+// Date
+// CTA
+
+// Use Intersection Observer
+
+// Click on CTA displays more informations
+
+console.log("Hello");
+
+let apiKey = "";
+
+var searchedMovie = document.getElementById("searchText").value;
+console.log(searchedMovie);
